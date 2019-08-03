4b25e32e9b593fa300fec22555b4853c1389b995
HowardLei <410905481@qq.com>
2019-08-03 09:59:00 +0800

    数据库连接成功

--- src/com/JavaWebDemo/database/Database.java
+++ src/com/JavaWebDemo/database/Database.java
@@ -1,5 +1,11 @@
 package com.JavaWebDemo.database;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Database class
  *
@@ -7,7 +13,59 @@ package com.JavaWebDemo.database;
  * @date 2019-08-02
  */
 public class Database {
+    private static final String LOGIN_USER = "root";
+    private static final String PASSWORD = "SweetieCan@0830";
     public static void main(String[] args) {
-
+        var url = "jdbc:mysql://localhost:3306/JavaWebDemoDatabase?useUnicode=true&characterEncoding=utf-8&allowPublicKeyRetrieval=true&useSSL=false&serverTimezone = GMT";
+        var mapList = new ArrayList<Map<String, String>>();
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection(url, LOGIN_USER, PASSWORD);
+            Statement statement = connection.createStatement();
+            String query = "select * from user;";
+            ResultSet resultSet = statement.executeQuery(query);
+            // 获得表当中的数据
+            while (resultSet.next()) {
+                String userName = resultSet.getString("userName");
+                String password = resultSet.getString("password");
+                var map = new HashMap<String, String>(1);
+                map.put(userName, password);
+                mapList.add(map);
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        mapList.forEach(stringStringMap -> System.out.println(stringStringMap));
+    }
+    public static boolean hasData(String userName, String password) {
+        var datas = getData();
+        for (Map<String, String> data : datas) {
+            if (data.get(userName) == password) {
+                return true;
+            }
+        }
+        return false;
+    }
+    private static List<Map<String, String>> getData() {
+        var url = "jdbc:mysql://localhost:3306/JavaWebDemoDatabase?useUnicode=true&characterEncoding=utf-8&allowPublicKeyRetrieval=true&useSSL=false&serverTimezone = GMT";
+        var mapList = new ArrayList<Map<String, String>>();
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection(url, LOGIN_USER, PASSWORD);
+            Statement statement = connection.createStatement();
+            String query = "select * from user;";
+            ResultSet resultSet = statement.executeQuery(query);
+            // 获得表当中的数据
+            while (resultSet.next()) {
+                String userName = resultSet.getString("userName");
+                String password = resultSet.getString("password");
+                var map = new HashMap<String, String>(1);
+                map.put(userName, password);
+                mapList.add(map);
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        return mapList;
     }
 }
--- src/com/JavaWebDemo/web/LoginServlet.java
+++ src/com/JavaWebDemo/web/LoginServlet.java
@@ -1,23 +1,40 @@
 package com.JavaWebDemo.web;
 
+import com.JavaWebDemo.database.Database;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
 
 /**
- * ${NAME} class
+ * LoginServlet class
  *
  * @author apple
  * @date 2019-08-02
  */
 @WebServlet(name = "LoginServlet", description = "登录的Servlet", urlPatterns = {"/login"})
 public class LoginServlet extends HttpServlet {
+    private static final String UTF8ENCODING = "utf-8";
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        request.setCharacterEncoding(UTF8ENCODING);
+        response.setCharacterEncoding(UTF8ENCODING);
+        var userName = request.getParameter("userName");
+        var password = request.getParameter("password");
+        var resultJsonMap = new HashMap<String, String>();
+        if (Database.hasData(userName, password)) {
+            resultJsonMap.put("\"code\"", "\"200\"");
+            resultJsonMap.put("\"message\"", "\"登录成功\"");
+        } else {
+            resultJsonMap.put("\"code\"", "\"5-0\"");
+            resultJsonMap.put("\"message\"", "\"登录失败\"");
+        }
+        var jsonObject = resultJsonMap.toString().replace('=', ':');
+        response.getWriter().append(jsonObject);
     }
 
     @Override
